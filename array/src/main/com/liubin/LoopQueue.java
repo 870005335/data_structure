1fa780182d14fa9e62b8141d724d41940620d8c3
Rouders <870005335@qq.com>
2019-10-17 23:39:41 +0800

    数据结构---循环队列的实现

---
+++ array/src/main/com/liubin/LoopQueue.java
@@ -0,0 +1,92 @@
+package main.com.liubin;
+
+public class LoopQueue<E> implements Queue<E> {
+
+    private E[] data;
+
+    private int front;
+    private int tail;
+    private int size;
+
+    public LoopQueue(int capacity) {
+        data = (E[]) new Object[capacity + 1];
+        front = 0;
+        tail = 0;
+        size = 0;
+    }
+
+    public LoopQueue() {
+        this(10);
+    }
+
+    public int getCapacity() {
+        return data.length - 1;
+    }
+
+    @Override
+    public void enQueue(E e) {
+        if ((tail + 1) % data.length == front) {
+            resize(getCapacity() *2);
+        }
+        data[tail] = e;
+        tail = (tail + 1) % data.length;
+        size ++;
+    }
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity + 1];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[(i + front) % data.length];
+        }
+        data = newData;
+        front = 0;
+        tail = size;
+    }
+
+    @Override
+    public E deQueue() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Queue is empty, can not deQueue");
+        }
+        E ret = data[front];
+        data[front] = null;
+        size--;
+        if (size == getCapacity() / 4 && getCapacity() / 2 != 0) {
+            resize(getCapacity() / 2);
+        }
+        return ret;
+    }
+
+    @Override
+    public E getFront() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Queue is empty");
+        }
+        return data[front];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return front == tail;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(String.format("Queue: size = %d, capacity = %d\n", size, getCapacity()));
+        sb.append("front [");
+        for (int i = front; i != tail; i = (i + 1) % data.length) {
+            sb.append(data[i]);
+            if ((i + 1) % data.length != tail) {
+                sb.append(", ");
+            }
+        }
+        sb.append("] tail");
+        return sb.toString();
+    }
+}
