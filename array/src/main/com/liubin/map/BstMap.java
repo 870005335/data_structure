014fda68d0fab8c6073bda35339330cd366a3a6d
870005335 <870005335@qq.com>
2019-11-18 23:36:36 +0800

    数据结构---基于二分搜索树的映射Map

--- array/src/main/com/liubin/map/BstMap.java
+++ array/src/main/com/liubin/map/BstMap.java
@@ -1,4 +1,7 @@
 package main.com.liubin.map;
+
+import org.w3c.dom.Node;
+
 /**
 * Title:BstMap.java
 * @discription: 基于二分搜索树的映射Map
@@ -6,38 +9,170 @@ package main.com.liubin.map;
 * @created 2019-11-18 17:39
 */
 public class BstMap<K extends Comparable<K>, V> implements Map<K, V> {
+
+	private Node root;
+	private int size;
+
+	public BstMap() {
+		root = null;
+		size = 0;
+	}
+
 	@Override
 	public void add(K key, V value) {
+		root = add(root, key, value);
+	}
 
+	private Node add(Node node, K key, V value) {
+		if (node == null) {
+			size++;
+			return new Node(key, value);
+		}
+		if (key.compareTo(node.key) < 0) {
+			node.left = add(node.left, key, value);
+		}else if (key.compareTo(node.key) > 0) {
+			node.right = add(node.right, key, value);
+		} else {
+			node.value = value;
+		}
+		return node;
+	}
+
+	/**
+	 * @MethodName: getNode
+	 * @Description: 返回以node节点为根的二分搜索树，key所在的节点
+	 * @param node
+	 * @param key
+	 * @Return: main.com.liubin.map.BstMap<K,V>.Node
+	 * @Author: Arthas_liubin@Foxmail.com
+	 * @Date: 2019/11/18 20:51
+	**/
+	private Node getNode(Node node, K key) {
+		if (node == null) {
+			return null;
+		}
+		if (node.key.equals(key)) {
+			return node;
+		} else if (node.key.compareTo(key) > 0) {
+			return getNode(node.left, key);
+		} else {
+			return getNode(node.right, key);
+		}
+	}
+
+	private Node getMin(Node node) {
+		if (node.left == null) {
+			return node;
+		}
+		return getMin(node.left);
+	}
+
+	private Node removeMin(Node node) {
+		if (node.left == null) {
+			Node right = node.right;
+			node.right = null;
+			size--;
+			return right;
+		}
+		node.left = removeMin(node.left);
+		return node;
 	}
 
 	@Override
 	public V remove(K key) {
+		Node node = getNode(root, key);
+		if (node != null) {
+			root = remove(root, key);
+			return node.value;
+		}
 		return null;
+	}
+
+	private Node remove(Node node, K key) {
+		if (node == null) {
+			return null;
+		}
+		if (key.compareTo(node.key) < 0) {
+			node.left = remove(node.left, key);
+			return node;
+		} else if (key.compareTo(node.key) > 0) {
+			node.right = remove(node.right, key);
+			return node;
+		} else {
+			if (node.left == null) {
+				Node right = node.right;
+				node.right = null;
+				size--;
+				return right;
+			}
+			if (node.right == null) {
+				Node left = node.left;
+				node.left = null;
+				size--;
+				return left;
+			}
+			Node successor = node.right;
+			successor.left = node.left;
+			successor.right = removeMin(node.right);
+			return successor;
+		}
 	}
 
 	@Override
 	public boolean contains(K key) {
-		return false;
+		Node node = getNode(root, key);
+		return node != null;
 	}
+
 
 	@Override
 	public V get(K key) {
-		return null;
+		return getNode(root, key).value;
 	}
 
 	@Override
 	public void set(K key, V value) {
-
+		Node node = getNode(root, key);
+		if (node != null) {
+			node.value = value;
+		}
 	}
 
 	@Override
 	public int getSize() {
-		return 0;
+		return size;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return false;
+		return size == 0;
+	}
+
+	private class Node {
+		public K key;
+		public V value;
+		public Node left;
+		public Node right;
+
+
+		public Node(K key, V value, Node left, Node right) {
+			this.key = key;
+			this.value = value;
+			this.left = left;
+			this.right = right;
+		}
+
+		public Node(K key, V value) {
+			this(key, value, null, null);
+		}
+
+		@Override
+		public String toString() {
+			final StringBuilder sb = new StringBuilder("Node{");
+			sb.append("key=").append(key);
+			sb.append(", value=").append(value);
+			sb.append('}');
+			return sb.toString();
+		}
 	}
 }
